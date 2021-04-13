8360e2a06ee8ffdacfa3e3c5028ee01fba61d0ea
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-13 16:56:43 +0200

    add array-filtrato

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 var cognomeUtente = prompt("Inserisci il tuo cognome")
+var listaCognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+var datiFiltrati = []
+var cognomeInserito = datiFiltrati.push(cognomeUtente);
+
+var i = 0;
+
+listaCognomi.sort();
+while (i < listaCognomi.length) {
+    var cognomeUtente = listaCognomi[i];
+    var cognomeEsistente = false
+
+    
+
+    for (var j = 0; j < datiFiltrati.length; j++)
+        var cognomeInserito = datiFiltrati[j];
+       
+
+    if (cognomeUtente === cognomeInserito) {
+        cognomeEsistente = true
+    }
+      
+    datiFiltrati.sort();
+    if (!cognomeEsistente) {
+        datiFiltrati.push(cognomeUtente)
+    }
+
+    i++
+}
+console.log("array originale", listaCognomi)
+console.log("array filtrato", datiFiltrati)
+
+
